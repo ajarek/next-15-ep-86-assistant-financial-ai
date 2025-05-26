8dce74769b1424b8ce36c1b36fb7bd924c88b91b
Ajarek <ajarek@poczta.onet.pl>
2025-05-26 22:10:39 +0200

    change changePassword

--- app/dashboard/changePassword/page.tsx
+++ app/dashboard/changePassword/page.tsx
@@ -1,15 +1,28 @@
 import UpdatePassword from '@/components/UpdatePassword'
 import React from 'react'
 
-const ChangePassword = async ({
-  searchParams,
-}: {
-  searchParams: { id: string }
-}) => {
-  const { id } = await searchParams
+interface ChangePasswordPageProps {
+  searchParams: {
+    id: string
+  }
+}
+
+const ChangePassword = ({ searchParams }: ChangePasswordPageProps) => {
+  const { id } = searchParams
+
+  if (!id) {
+    return (
+      <div className='flex flex-col items-center justify-center h-screen gap-4 px-4'>
+        <h1 className='text-2xl font-semibold text-red-500'>
+          Błąd: Nie podano identyfikatora użytkownika.
+        </h1>
+        <p>Upewnij się, że posiadasz ważny link umożliwiający zmianę hasła.</p>
+      </div>
+    )
+  }
   return (
     <div className=' flex flex-col items-center justify-center h-screen gap-4 px-4'>
-      <h1 className='text-2xl font-semibold'>Change Password</h1>
+      <h1 className='text-2xl font-semibold'>Zmień hasło</h1>
       <UpdatePassword id={id} />
     </div>
   )
