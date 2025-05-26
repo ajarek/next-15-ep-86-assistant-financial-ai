5d68a9267d91e2f025ff332243da20ec84348ca9
Ajarek <ajarek@poczta.onet.pl>
2025-05-26 21:54:49 +0200

    change FormUpdateUser

--- components/FormUpdateUser.tsx
+++ components/FormUpdateUser.tsx
@@ -4,7 +4,7 @@ import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { updateUser } from '@/lib/action'
-import { signOut } from '@/app/api/auth/auth'
+
 
 type FormUpdateUserProps = {
   idUpdate: string
